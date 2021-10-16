41f57bc4e5a2100fa6b99003bdb65e4719be8031
songzeming <songzemings@gmail.com>
2021-10-16 17:54:12 +0800

    fix: 优化热更文件对比工具

--- diff_hot_update/tools/diff.py
+++ diff_hot_update/tools/diff.py
@@ -8,17 +8,21 @@ filesnew = {}
 filesold = {}
 filelist = {}
 diffsize = 0
+dynamicsize = 0
+
 
 def turn_mb(b):
     return round(b / 1024 / 1024, 2)
- 
+
+
 def getmd5(file):
     m = hashlib.md5()
-    with open(file,'rb') as f:
+    with open(file, 'rb') as f:
         for line in f:
             m.update(line)
     md5code = m.hexdigest()
     return md5code
+
 
 def insert_files(file, t):
     if t == "new":
@@ -26,17 +30,20 @@ def insert_files(file, t):
     else:
         filesold[os.path.basename(file)] = file
 
+
 def find_file(path, t):
-	dirs = os.listdir(path)
-	for i in range(0, len(dirs)):
-		file = os.path.join(path, dirs[i])
-		if os.path.isdir(file):
-			find_file(file, t)
-		elif os.path.isfile(file):
-			insert_files(file, t)
+    dirs = os.listdir(path)
+    for i in range(0, len(dirs)):
+        file = os.path.join(path, dirs[i])
+        if os.path.isdir(file):
+            find_file(file, t)
+        elif os.path.isfile(file):
+            insert_files(file, t)
+
 
 find_file("./old", "old")
 find_file("./new", "new")
+
 
 def calculate(name, file, desc):
     filesize = os.path.getsize(file)
@@ -49,21 +56,31 @@ def calculate(name, file, desc):
                 filelist[filesplit5] = 0
             filelist[filesplit5] += filesize
     fileshortname = filefullname.replace('./new/1.1/Bundles/Android', '')
-    print('%-32s%-8s%-6s%-6s%-20s' %(fileshortname, desc, turn_mb(filesize), "MB", name))
-    return filesize
+    print('%-32s%-8s%-6s%-6s%-20s' % (fileshortname, desc, turn_mb(filesize), "MB", name))
+    return filesize, fileshortname.startswith("/dynamicres")
+
 
 for name, file in filesnew.items():
     if filesold.get(name, None) != None:
         if getmd5(file) != getmd5(filesold[name]):
-            diffsize += calculate(name, file, "add")
+            f_size, f_dynamic = calculate(name, file, "add")
+            if f_dynamic:
+                dynamicsize += f_size
+            else:
+                diffsize += f_size
     else:
-        diffsize += calculate(name, file, "diff")
+        f_size, f_dynamic = calculate(name, file, "diff")
+        if f_dynamic:
+            dynamicsize += f_size
+        else:
+            diffsize += f_size
 
 print("")
-    
+
 for name, size in filelist.items():
-    print('%-20s%-6s%-6s' %(name, turn_mb(size), "MB"))
-    
+    print('%-20s%-6s%-6s' % (name, turn_mb(size), "MB"))
+
 print("")
-print("热更大小:", diffsize, "B")
 print("热更大小:", turn_mb(diffsize), "MB")
+print("动态资源大小:", turn_mb(dynamicsize), "MB")
+print("热更总大小:", turn_mb(diffsize + dynamicsize), "MB")
