7984dc6bcdb21376494e8455bd2962cdf3cca6c3
xiaoze <437816240@qq.com>
2021-12-27 10:20:51 +0800

    fix: 优化cc分支热更文件对比工具

--- diff_hot_update/tools/copy.py
+++ diff_hot_update/tools/copy.py
@@ -8,4 +8,4 @@ hot_update_path = "../../jenkins/workspace/p601_release/HotUpdate/"
 dirs = os.listdir(hot_update_path)
 
 shutil.rmtree("diff_folder/new", ignore_errors=True)
-shutil.copytree("{path}/{version}/Bundles/".format(path=hot_update_path, version=dirs[1]), "diff_folder/new/")
+shutil.copytree("{path}/{version}/Bundles/".format(path=hot_update_path, version=dirs[0]), "diff_folder/new/")
--- diff_hot_update/tools/save.py
+++ diff_hot_update/tools/save.py
@@ -8,7 +8,7 @@ hot_update_path = "../../jenkins/workspace/p601_release/HotUpdate/"
 dirs = os.listdir(hot_update_path)
 
 shutil.rmtree("diff_folder/new/", ignore_errors=True)
-shutil.copytree("{path}/{version}/Bundles/".format(path=hot_update_path, version=dirs[1]), "diff_folder/new/")
+shutil.copytree("{path}/{version}/Bundles/".format(path=hot_update_path, version=dirs[0]), "diff_folder/new/")
 
 shutil.rmtree("diff_folder/old/", ignore_errors=True)
-shutil.copytree("{path}/{version}/Bundles/".format(path=hot_update_path, version=dirs[1]), "diff_folder/old/")
+shutil.copytree("{path}/{version}/Bundles/".format(path=hot_update_path, version=dirs[0]), "diff_folder/old/")
